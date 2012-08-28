bda96b7a051c1d77d2bd2168cf4dff5cd089f7c0
induk <indirakorrapati@gmail.com>
2012-08-28 15:26:48 -0400

    Initial Commit

---
+++ app/models/book.rb
@@ -0,0 +1,6 @@
+class Book < ActiveRecord::Base
+  # attr_accessible :title, :body
+  belongs_to :subject
+  validates_presence_of :title
+  validates_numericality_of :price, :message=>"Error Message"
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Library::Application.config.secret_token = 'b809513e2cfcaf6f865e982dc3cd474c55364f3cf33c5104c0900b9c9b8c701f9cf06e2c0601fc9b31238ed2e11bcdccdd727c82dd3599b459aeeb5e37dea00f'
